331a3eea50c016f202aea876607ff98667515d06
Shivakumar Kamtala <skamtala@gmail.com>
2018-09-26 21:10:05 -0400

    my first  repo

---
+++ src/main/java/samples/java/JavaTest.java
@@ -0,0 +1,11 @@
+package samples.java;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Hellow World");
+
+	}
+
+}
